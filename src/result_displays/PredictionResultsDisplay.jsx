deb601b76cc0376bd7fd86e0685e0d934c576652
e.hokage777 <e.hokage777@gmail.com>
2023-12-18 13:49:10 +0000

    updated prediction result display

--- src/result_displays/PredictionResultsDisplay.jsx
+++ src/result_displays/PredictionResultsDisplay.jsx
@@ -5,7 +5,7 @@ export default function PredictionResultsDisplay(props) {
   return (
     <div>
       <CircularLoader size="200" value={props.probability}>
-        {parseFloat(props.probability).toFixed(2) * 100}%
+        {(parseFloat(props.probability).toFixed(2) * 100).toString().substring(0,4)}%
         <span style={{ fontSize: "10px", color: predictionColor }}>
           {props.prediction ? "Positive" : "Negative"}
         </span>
